c05ea1f2b5040f405273d70a2f6ef9070696b2d7
Benji Altman <benji@altmansoftwaredesign.com>
2018-01-05 13:42:59 -0600

    keep up to date

--- bathroomNoise.py
+++ bathroomNoise.py
@@ -1,29 +1,47 @@
+#!/usr/bin/env python
 import RPi.GPIO as GPIO
 import time #needed for sleep
 import os   #needed for os calls
 from multiprocessing import Process
+from random import *
 
+
+location = "/home/pi/soundArtJS/"
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(2, GPIO.IN)
 
-fileName = "/home/pi/soundArtJS/public_html/index.html"
+fileName = "public_html/index.html"
 cycleTime = 358 * 4 * 20 / 1000
 cycles = 2
 runTime = cycles * cycleTime
+rickChance = 128
 #print(runTime)
 
 def otherProcess():
-    os.system("chromium-browser " + fileName)
+    os.system("chromium-browser " + location + fileName)
+
+rickTime = 3 * 60 + 31
+def rickRoll():
+    os.system("vlc " + location + "Never\ Gonna\ Give\ You\ Up\ Original.mp3")
 
 def buttonPressHandler():
-    p = Process(target=otherProcess)
-    #print('hello')
-    p.start()
-    #print('world')
-    time.sleep(runTime)
-    os.system("kill $(ps aux | grep '[c]hromium-browser' | awk '{print $2}')")
-    p.join()
+    print("hi")
+    if randint(1, rickChance) == 1:
+        p = Process(target=rickRoll)
+        p.start()
+        time.sleep(rickTime + 5)
+        os.system("kill $(ps aux | grep '[v]lc' | awk '{print $2}')")
+        p.join()
+    else:
+        p = Process(target=otherProcess)
+        #print('hello')
+        p.start()
+        #print('world')
+        time.sleep(runTime)
+        os.system("kill $(ps aux | grep '[c]hromium-browser' | awk '{print $2}')")
+        p.join()
+buttonPressHandler()
 
 #Run loop
 while True:
